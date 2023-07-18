9a3d0e44f28c0c5386731f49b41b78a9dde7d5c7
Andrew T <me@andrewtran-shi.dev>
2023-07-18 12:25:15 -0700

    modified:   client/src/components/AuthorList.jsx 	modified:   client/src/components/New.jsx

--- client/src/components/AuthorList.jsx
+++ client/src/components/AuthorList.jsx
@@ -3,71 +3,71 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const AuthorList = () => {
-    const [authors, setAuthors] = useState([]);
+	const [authors, setAuthors] = useState([]);
 
-    const deleteAuthor = (authorId) => {
-        axios
-            .delete("http://localhost:8000/api/author/" + authorId)
-            .then(() => removeFromDom(authorId))
-            .catch((err) => console.error(err));
-    };
+	const deleteAuthor = (authorId) => {
+		axios
+			.delete("http://localhost:8000/api/author/" + authorId)
+			.then(() => removeFromDom(authorId))
+			.catch((err) => console.error(err));
+	};
 
-    const removeFromDom = (authorId) => {
-        setAuthors(authors.filter((author) => author._id !== authorId));
-    };
+	const removeFromDom = (authorId) => {
+		setAuthors(authors.filter((author) => author._id !== authorId));
+	};
 
-    useEffect(() => {
-        axios
-            .get("http://localhost:8000/api/author")
-            .then((res) => {
-                const sortedAuthors = res.data.sort((a, b) =>
-                    a.name.localeCompare(b.name)
-                );
-                setAuthors(sortedAuthors);
-            })
-            .catch((err) => console.error(err));
-    }, []);
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/api/author")
+			.then((res) => {
+				const sortedAuthors = res.data.sort((a, b) =>
+					a.name.localeCompare(b.name)
+				);
+				setAuthors(sortedAuthors);
+			})
+			.catch((err) => console.error(err));
+	}, []);
 
-    return (
-        <>
-            <h1 className="text-2xl text-red-400 my-2 font-light">
-                We have quotes by :
-            </h1>
-            <div className="overflow-x-auto">
-                <table className="table text-center">
-                    <thead>
-                        <tr>
-                            <th className="text-accent text-2xl font-light">Author</th>
-                            <th className="text-focus text-2xl font-light">
-                                Actions Available
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {authors.map((author) => (
-                            <tr className="bg-content" key={author._id}>
-                                <td className="text-xl font-semibold text-secondary-focus">
-                                    {author.name}
-                                </td>
-                                <td>
-                                    <Link
-                                        to={`/author/${author._id}/edit`}
-                                        className="btn btn-sm rounded mx-1 bg-success">
-                                        Edit
-                                    </Link>
-                                    <button
-                                        onClick={() => deleteAuthor(author._id)}
-                                        className=" btn btn-sm rounded mx-1 bg-warning">
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<h1 className="text-2xl text-red-400 my-2 font-light">
+				We have quotes by :
+			</h1>
+			<div className="overflow-x-auto">
+				<table className="table text-center">
+					<thead>
+						<tr>
+							<th className="text-accent text-2xl font-light">Author</th>
+							<th className="text-focus text-2xl font-light">
+								Actions Available
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{authors.map((author) => (
+							<tr className="bg-content" key={author._id}>
+								<td className="text-xl font-semibold text-secondary-focus">
+									{author.name}
+								</td>
+								<td>
+									<Link
+										to={`/author/${author._id}/edit`}
+										className="btn btn-sm rounded mx-1 bg-success">
+										Edit
+									</Link>
+									<button
+										onClick={() => deleteAuthor(author._id)}
+										className=" btn btn-sm rounded mx-1 bg-warning">
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 };
 
 export default AuthorList;
--- client/src/components/New.jsx
+++ client/src/components/New.jsx
@@ -2,34 +2,34 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const New = () => {
-    const [name, setName] = useState("");
-    const navigate = useNavigate();
+	const [name, setName] = useState("");
+	const navigate = useNavigate();
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        axios
-            .post("http://localhost:8000/api/author", { name })
-            .then(() => navigate("/"))
-            .catch((err) => console.log(err));
-    };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post("http://localhost:8000/api/author", { name })
+			.then(() => navigate("/"))
+			.catch((err) => console.log(err));
+	};
 
-    return (
-        <form onSubmit={onSubmit}>
-            		<div className="form-control w-full max-w-xs mx-auto">
-					<label className="label">
-						<span className="label-text">Favorite Author's name?</span>
-					</label>
-					<input
-						type="text"
-						placeholder="Type here"
-						className="input input-bordered input-success w-full min-w-xs"
-                              onChange={(e) => setName(e.target.value)}
-                    value={name}
-      />
-            <button type="submit">Submit</button>
-            </div>
-        </form>
-    );
+	return (
+		<form onSubmit={onSubmit}>
+			<div className="form-control w-full max-w-xs mx-auto">
+				<label className="label">
+					<span className="label-text">Favorite Author's name?</span>
+				</label>
+				<input
+					type="text"
+					placeholder="Type here"
+					className="input input-bordered input-success w-full min-w-xs"
+					onChange={(e) => setName(e.target.value)}
+					value={name}
+				/>
+				<button type="submit">Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default New;
